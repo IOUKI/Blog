33967b43eb67efad896615e5b360da226565e28f
張程凱 <lan_dev@zhangchengkaideMacBook-Air.local>
2022-09-25 21:39:11 +0800

    section top 大略製作完畢

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 AOS.init();
 AOS.refresh();
+
+const contentLink = document.getElementById('scrollDownBtn');
+contentLink.addEventListener('click', () => {
+    const contentAnchor = document.getElementById('main-content-top');
+    contentAnchor.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end'
+    });
+});
